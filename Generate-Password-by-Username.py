d844bd8ffa6ba6cc72d0fd36fa4ee0d5a18e3a7a
Mohammed Djeouadi <69479417+moha999DJ@users.noreply.github.com>
2024-01-30 19:08:07 +0100

    generate password by username using python
    Suggestion of Password by using username to create a strong password

---
+++ Generate-Password-by-Username.py
@@ -0,0 +1,14 @@
+import random
+import string
+strings=string.digits+string.punctuation
+print("Welcome to my app ")
+user_name=input("Please enter your username :")
+pure_user=""
+for password in user_name:
+    if password not in strings:
+     pure_user+=password
+     sugg=random.choices(strings,k=random.randint(1,8))
+     sugg_password=[pure_user]+sugg
+     random.shuffle(sugg_password)     
+print(f"\nThis is your pure username :{pure_user}\n")
+print(f"The password you can use is : {"".join(sugg_password)} ")
